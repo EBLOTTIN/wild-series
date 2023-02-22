a1240f86dac844ac36f0c351d9467d7fdd5ac369
Enzo BLOTTIN <enzoblottin@gmail.com>
2023-02-22 14:50:22 +0100

    fin de l'exo 9

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -1,22 +1,27 @@
 <?php
 
+
 namespace App\DataFixtures;
 
+
 use App\Entity\Category;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
+
 use Doctrine\Persistence\ObjectManager;
 
 
 class CategoryFixtures extends Fixture
+
 {
-    const CATEGORIES = [
+
+    public const CATEGORIES = [
         'Action',
         'Aventure',
         'Animation',
         'Fantastique',
         'Horreur',
     ];
-
     public function load(ObjectManager $manager)
     {
         foreach (self::CATEGORIES as $categoryName) {
